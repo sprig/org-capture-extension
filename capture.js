5a2dcaaeaa023974ad4b5ababf83308f969b6b65
Samuel W. Flint <swflint@flintfam.org>
2017-06-30 11:25:26 -0500

    Started updating the package

--- capture.js
+++ capture.js
@@ -3,20 +3,7 @@ var capture = function(){
     var esc = function (text) { return replace_all(replace_all(replace_all(encodeURIComponent(text),"[(]",escape("(")),"[)]",escape(")")),"[']",escape("'")); };
     var selection = window.getSelection().toString();
 
-    var uri = 'org-protocol://';
-    if (selection != "")
-    {
-	uri += 'capture:/p/';
-    }
-    else
-    {
-	uri += 'capture:/L/'
-    };
-
-    uri += encodeURIComponent(location.href) + '/' +
-	esc(document.title) ;
-
-    if (selection != "") { uri += '/' + esc(selection); };
+    var uri = 'org-protocol://capture?template=b&url=' + encodeURIComponent(location.href) + '&title=' + esc(document.title) ;
 
     console.log("Capturing the following URI with org-protocol:", uri);
 
