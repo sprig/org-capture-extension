2286b22f9860ce7571487510de3371747efcda4a
Samuel W. Flint <swflint@flintfam.org>
2017-07-04 10:09:26 -0500

    Removed extraneous, duplicate code

--- background.js
+++ background.js
@@ -1,20 +1,3 @@
-function replace_all(str, find, replace) {
-    return str.replace(new RegExp(find, 'g'), replace);
-}
-
-function escapeIt(text) {
-    return replace_all(replace_all(replace_all(encodeURIComponent(text), "[(]", escape("(")),
-                                   "[)]", escape(")")),
-                       "[']" ,escape("'"));
-}
-
-function createStoreURL(title, url, oldStyle) {
-    if (oldStyle == true)
-        return "org-protocol://store-link:/"+url+"/"+title;
-    else
-        return "org-protocol://store-link?url="+url+'&title='+title;
-}
-
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({file: "capture.js"}, (function (url_array) {}));
 });
