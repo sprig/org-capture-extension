6735324b159d68106be5a77295ff2782e1074fb7
Konstantin Kliakhandler <kosta@slumpy.org>
2017-08-16 22:45:39 -0400

    Bumped extension version and added update logic.

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "install")
+        chrome.storage.sync.set(
+            {
+                selectedTemplate: 'L',
+                unselectedTemplate: 'p',
+                useNewStyleLinks: true,
+		debug: false
+            });
+    else if ((details.reason == "update" && details.previousVersion.startsWith("0.1")))
+        chrome.storage.sync.set(
+            {
+                selectedTemplate: 'L',
+                unselectedTemplate: 'p',
+                useNewStyleLinks: false,
+		debug: false
+            });
+});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({file: "capture.js"}, (function (url_array) {}));
 });
