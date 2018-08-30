7094b8583e3853ffe9212816bb4ba89a440cc23a
Sean Farley <sean@farley.io>
2018-08-29 19:26:38 -0700

    content: use bindGlobal so shortcut works in text fields

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.storage.sync.get({
   shortcut: "ctrl+c c"
 }, function(options) {
-  Mousetrap.bind(options.shortcut, function() {
+  Mousetrap.bindGlobal(options.shortcut, function() {
     chrome.runtime.sendMessage("orgcapture");
   });
 });
